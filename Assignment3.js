8f1645ef4ea9530c759a35bef9d4057b713bc587
shanjune260686 <shanjune86@gmail.com>
2025-11-22 23:18:11 -0400

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,14 @@
+
+function isOddOrEven(number){
+    if (number>0){
+        console.log("Positive Number")
+    }
+    else if(number < 0){
+        console.log("Negative number")
+    }
+    else{
+        console.log("Neutral")
+    }
+}
+let number =0
+isOddOrEven(number)
